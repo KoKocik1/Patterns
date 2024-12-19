0a8b6197b5aea52fb6804d77ec304d732b40f269
Krzysztof Kokot <krzysiukokot@interia.pl>
2024-12-19 21:49:03 +0100

    Solid L

--- Solid_L/Program.cs
+++ Solid_L/Program.cs
@@ -1,3 +1,20 @@
-﻿
-namespace Solid_L;
+﻿namespace Solid_L;
 
+public class Program
+{
+    static public int Area(Rectangle rc) => rc.Width * rc.Height;
+
+    public static void Main()
+    {
+        Rectangle rc = new Rectangle(2, 3);
+        Console.WriteLine($"{rc} has area {Area(rc)}");
+
+        //Square sq = new Square(); // Works as expected
+        
+        // To work as expected, the Square class
+        // should override the Width and Height properties
+        Rectangle sq = new Square();
+        sq.Width = 4;
+        Console.WriteLine($"{sq} has area {Area(sq)}");
+    }
+}
---
+++ Solid_L/Square.cs
@@ -0,0 +1,14 @@
+namespace Solid_L;
+
+public class Square : Rectangle
+{
+    public override int Width
+    {
+        set => base.Width = base.Height = value;
+    }
+
+    public override int Height
+    {
+        set => base.Width = base.Height = value;
+    }
+}
