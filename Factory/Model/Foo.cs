40249a1978eac7320cf3f04d05e6edc650022ffb
Krzysztof Kokot <krzysiukokot@interia.pl>
2024-12-29 22:54:58 +0100

    Factory: Async

---
+++ Factory/Model/Foo.cs
@@ -0,0 +1,26 @@
+namespace Factory.Model;
+
+public class Foo
+{
+    private Foo()
+    {
+        
+    }
+    
+    private async Task<Foo> InitAsync()
+    {
+        await Task.Delay(1000);
+        return this;
+    }
+    public static Task<Foo> CreateAsync()
+    {
+        var result = new Foo();
+        return result.InitAsync();
+    }
+
+    public override string ToString()
+    {
+        return "Foo instance";
+    }
+
+}
--- Factory/Program.cs
+++ Factory/Program.cs
@@ -2,13 +2,16 @@
 
 public class Program
 {
-    public static void Main()
+    public static async Task Main(string[] args)
     {
         var point = Point.NewPolarPoint(1.0, Math.PI / 2);
         Console.WriteLine(point);
         
         var point2 = Point.NewCartesianPoint(1, 2);
         Console.WriteLine(point2);
+        
+        Foo x = await Foo.CreateAsync();
+        Console.WriteLine(x.ToString());
     }
 
 }
