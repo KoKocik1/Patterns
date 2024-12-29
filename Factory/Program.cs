7407a0bf60baee92639f22969c27c9b20f01ce1d
Krzysztof Kokot <krzysiukokot@interia.pl>
2024-12-29 23:40:13 +0100

    Factory: Refactoring

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -5,20 +5,25 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
+        // simple example
         var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
         Console.WriteLine(point);
         
+        // simple example
         var point2 = PointFactory.NewCartesianPoint(1, 2);
         Console.WriteLine(point2);
         
+        //async example
         Foo x = await Foo.CreateAsync();
         Console.WriteLine(x.ToString());
 
+        // Additional data about created objects
         var factory = new TrackingThemeFactory();
         var theme1 = factory.CreateTheme(true);
         var theme2 = factory.CreateTheme(false);
         Console.WriteLine(factory.Info);
         
+        // Replace data in created objects
         var factory2 = new ReplacableThemeFactory();
         var magicTheme = factory2.CreateTheme(true);
         Console.WriteLine(magicTheme.Value.BgrColor);
