337b195b2255309bea56bcdb70684022c58012ff
Krzysztof Kokot <krzysiukokot@interia.pl>
2024-12-29 22:48:06 +0100

    Factory: Simple example

---
+++ Factory/Program.cs
@@ -0,0 +1,14 @@
+﻿using Factory.Model;
+
+public class Program
+{
+    public static void Main()
+    {
+        var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+        Console.WriteLine(point);
+        
+        var point2 = Point.NewCartesianPoint(1, 2);
+        Console.WriteLine(point2);
+    }
+
+}
