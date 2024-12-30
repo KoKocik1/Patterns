354d57c2a24042d682031c1de924fe4d8aa5ba6d
Krzysztof Kokot <krzysiukokot@interia.pl>
2024-12-30 22:11:07 +0100

    Singleton: One class per thread

---
+++ Singleton/Models/PerThreadSingleton.cs
@@ -0,0 +1,17 @@
+namespace Singleton.Models;
+
+public sealed class PerThreadSingleton
+{
+    private static ThreadLocal<PerThreadSingleton> threadInstance
+        = new ThreadLocal<PerThreadSingleton>(
+            () => new PerThreadSingleton());
+
+    public int Id;
+    
+    private PerThreadSingleton()
+    {
+        Id = Thread.CurrentThread.ManagedThreadId;
+    }
+
+    public static PerThreadSingleton Instance => threadInstance.Value;
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -36,6 +36,19 @@ namespace Singleton
 
       var ceo2 = new ChiefExecutiveOfficer();
       WriteLine(ceo2);
+      
+      // Singleton per thread
+      var t1 = Task.Factory.StartNew(() =>
+      {
+        Console.WriteLine($"t1: " + PerThreadSingleton.Instance.Id);
+      });
+      var t2 = Task.Factory.StartNew(() =>
+      {
+        Console.WriteLine($"t2: " + PerThreadSingleton.Instance.Id);
+        Console.WriteLine($"t2 again: " + PerThreadSingleton.Instance.Id);
+      });
+      Task.WaitAll(t1, t2);
+    
     }
   }
 }
