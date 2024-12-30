72596bb932a39cc8d5b3766e3df26a38f7f32374
Krzysztof Kokot <krzysiukokot@interia.pl>
2024-12-30 21:53:28 +0100

    Singleton: unit tests

---
+++ SingletonTests/Mock/DummyDatabase.cs
@@ -0,0 +1,16 @@
+using Singleton.Interfaces;
+
+namespace Singleton.Tests;
+
+public class DummyDatabase : IDatabase
+{
+    public int GetPopulation(string name)
+    {
+        return new Dictionary<string, int>
+        {
+            ["alpha"] = 1,
+            ["beta"] = 2,
+            ["gamma"] = 3
+        }[name];
+    }
+}
---
+++ SingletonTests/Program.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Singleton.Database;
+using Singleton.Tests;
+using Singleton.Utils;
+
+[TestFixture]
+public class SingletonTests
+{
+    [Test]
+    public void IsSingletonTest()
+    {
+        var db = SingletonDatabase.Instance;
+        var db2 = SingletonDatabase.Instance;
+        Assert.That(db, Is.SameAs(db2));
+        Assert.That(SingletonDatabase.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void SingletonTotalPopulationTest()
+    {
+        // testing on a live database
+        var rf = new SingletonRecordFinder();
+        var names = new[] {"Seoul", "Mexico City"};
+        int tp = rf.TotalPopulation(names);
+        Assert.That(tp, Is.EqualTo(17500000 + 17400000));
+    }
+
+    [Test]
+    public void DependantTotalPopulationTest()
+    {
+        var db = new DummyDatabase();
+        var rf = new ConfigurableRecordFinder(db);
+        Assert.That(
+            rf.GetTotalPopulation(new[]{"alpha", "gamma"}),
+            Is.EqualTo(4));
+    }
+}
