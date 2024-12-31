18fd59f00f54185deeac2e6f6b82ad291591e289
Krzysztof Kokot <krzysiukokot@interia.pl>
2024-12-31 07:08:26 +0100

    Adapter: Simple example with caching

---
+++ Adapter/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Adapter.Model;
+using Adapter.Utils;
+
+class Program
+{
+    private static readonly List<VectorObject> vectorObjects = new List<VectorObject>
+    {
+        new VectorRectangle(1, 1, 10, 10),
+        new VectorRectangle(3, 3, 6, 6)
+    };
+    
+    static void Main(string[] args)
+    {
+        DrawUtils.Draw(vectorObjects);
+        DrawUtils.Draw(vectorObjects);
+    }
+}
