34e1438597ef1c504196a4c33dc41da7c957d48d
Krzysztof Kokot <krzysiukokot@interia.pl>
2024-12-31 11:36:01 +0100

    Adamter: csproj for tests

---
+++ AdapterTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using Adapter.Adapter;
+using Adapter.Model;
+using NUnit.Framework;
+
+[TestFixture]
+public class SingletonTests
+{
+    [Test]
+    public void IsSingletonTest()
+    {
+        var square = new Square(5);
+        var rectangle = new SquareToRectangleAdapter(square);
+        Assert.That(rectangle.Width, Is.EqualTo(5));
+        Assert.That(rectangle.Height, Is.EqualTo(5));
+    }
+}
